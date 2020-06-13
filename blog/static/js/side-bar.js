9d6791e3fe99b7383eaf4cadf3c03cf1a3a00cf9
Adrian <adrian@code.je>
2020-06-13 20:04:22 +0100

    just a commit

--- blog/static/js/side-bar.js
+++ blog/static/js/side-bar.js
@@ -2,3 +2,8 @@ $('.custom-dropdown-button').on('click', e => {
     const toggleId = `#${e.target.dataset.toggle}`
     $(toggleId).toggle()
 })
+
+$('.login-dropdown-button').on('click', e => {
+    const toggleId = `#${e.target.dataset.toggle}`
+    $(toggleId).toggle()
+})
