d931402dca4dfc2d95ad7cbf46b74143a67beaa1
Adrian <adrian@code.je>
2020-06-14 10:23:01 +0100

    sidebar social login added

--- blog/static/js/side-bar.js
+++ blog/static/js/side-bar.js
@@ -2,8 +2,3 @@ $('.custom-dropdown-button').on('click', e => {
     const toggleId = `#${e.target.dataset.toggle}`
     $(toggleId).toggle()
 })
-
-$('.login-dropdown-button').on('click', e => {
-    const toggleId = `#${e.target.dataset.toggle}`
-    $(toggleId).toggle()
-})
