30b66b7fa1a6c3750148f4b45c9f9e20ed65022a
Adrian <adrian@code.je>
2020-08-15 21:57:52 +0100

    added post readtime

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PROTOCOL=https
 # export SITE_NAME=localhost
-export SITE_NAME=051f1b16b833.ngrok.io
+export SITE_NAME=9077f5c83d1d.ngrok.io
 
 DJANGO_SETTINGS_MODULE=bliss_blog.settings ~/environment/bliss_blog_env/bin/python manage.py runserver
